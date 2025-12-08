66c2b6e553d54e7f5acac33e13d4dc16dc1fc0e4
Pedro Jiménez Guerrero <pedjimgue1@gmail.com>
2025-12-08 23:48:16 +0100

    fix: force db upgrade execution on render entrypoint

--- docker/entrypoints/render_entrypoint.sh
+++ docker/entrypoints/render_entrypoint.sh
@@ -22,34 +22,8 @@ if [ ! -d "migrations/versions" ]; then
     flask db migrate
 fi
 
-# Check if the database is empty
-if [ $(mariadb -u $MARIADB_USER -p$MARIADB_PASSWORD -h $MARIADB_HOSTNAME -P $MARIADB_PORT -D $MARIADB_DATABASE -sse "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$MARIADB_DATABASE';") -eq 0 ]; then
- 
-    echo "Empty database, migrating..."
-
-    # Get the latest migration revision
-    LATEST_REVISION=$(ls -1 migrations/versions/*.py | grep -v "__pycache__" | sort -r | head -n 1 | sed 's/.*\/\(.*\)\.py/\1/')
-
-    echo "Latest revision: $LATEST_REVISION"
-
-    # Run the migration process to apply all database schema changes
-    flask db upgrade
-
-else
-
-    echo "Database already initialized, updating migrations..."
-
-    # Get the current revision to avoid duplicate stamp
-    CURRENT_REVISION=$(mariadb -u $MARIADB_USER -p$MARIADB_PASSWORD -h $MARIADB_HOSTNAME -P $MARIADB_PORT -D $MARIADB_DATABASE -sse "SELECT version_num FROM alembic_version LIMIT 1;")
-    
-    if [ -z "$CURRENT_REVISION" ]; then
-        # If no current revision, stamp with the latest revision
-        flask db stamp head
-    fi
-
-    # Run the migration process to apply all database schema changes
-    flask db upgrade
-fi
+echo "Running database migrations..."
+flask db upgrade
 
 # Start the application using Gunicorn, binding it to port 80
 # Set the logging level to info and the timeout to 3600 seconds
