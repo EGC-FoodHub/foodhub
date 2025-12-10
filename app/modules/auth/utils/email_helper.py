fff1ed61ff8e9428651778ad029cb72ac3ce8cbc
Miguel Garcia Vizcaino <miguelgvizcaino@gmail.com>
2025-12-10 19:21:19 +0100

    feat(fixing linter rosemary): :sparkles: changing resend for brevo in change password

--- app/modules/auth/utils/email_helper.py
+++ app/modules/auth/utils/email_helper.py
@@ -1,4 +1,5 @@
 import os
+import secrets
 
 import requests
 from dotenv import load_dotenv
@@ -72,6 +73,68 @@ def send_email_verification(user: User):
         "sender": {"name": SENDER_NAME, "email": SENDER_EMAIL},
         "to": [{"email": user.email}],
         "subject": "Verify your FoodHub account",
+        "htmlContent": html_template,
+    }
+
+    try:
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()
+        print(f"Verification email sent to {user.email}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error sending verification email: {e}")
+
+def send_password_change_email(user: User):
+    if not BREVO_API_KEY:
+        print("Error: BREVO_API_KEY no está configurada")
+        return False
+
+    if not SENDER_EMAIL:
+        print("Error: SENDER_EMAIL no está configurada")
+        return False
+
+    token = secrets.token_hex(6)
+    os.environ["TOKEN_KEY"] = token
+
+    html_template = f"""
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8" />
+        <title>FoodHub - Change your password</title>
+      </head>
+      <body style="font-family: Arial, sans-serif; background-color: #f6f8fa; padding: 40px;">
+        <div style="max-width: 600px; margin: auto; background: white; padding: 30px; border-radius: 12px;
+                    box-shadow: 0 4px 10px rgba(0,0,0,0.08); border-top: 6px solid #0366d6;">
+          <h2 style="text-align:center; color:#0366d6; margin-bottom: 20px; font-weight: 600;">
+            Hello, {user.profile.name} {user.profile.surname}! 🍽️
+          </h2>
+
+          <p style="color:#444; font-size: 16px; line-height: 1.6;">
+            Hi {user.profile.name}, here is your code for changing your password.
+          </p>
+
+          <p style="color:#555; font-size:15px; line-height:1.6; text-align:center;">
+            If the button above doesn’t work, copy and paste this link into your browser:<br>
+            <a style="color:#0366d6; word-break: break-all;">{token}</a>
+          </p>
+
+          <hr style="border: none; border-top: 1px solid #e1e4e8; margin: 30px 0;">
+
+          <p style="color:#777; text-align:center; font-size:12px; line-height:1.5;">
+            If you didn’t ask for changing your FoodHub, you can safely ignore this email.
+          </p>
+        </div>
+      </body>
+    </html>
+    """
+
+    url = "https://api.brevo.com/v3/smtp/email"
+    headers = {"accept": "application/json", "api-key": BREVO_API_KEY, "content-type": "application/json"}
+
+    data = {
+        "sender": {"name": SENDER_NAME, "email": SENDER_EMAIL},
+        "to": [{"email": user.email}],
+        "subject": "Change your account's password of FoodHub",
         "htmlContent": html_template,
     }
 
