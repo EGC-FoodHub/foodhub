14325b92971ee33f2a2fe959e8455a7ed3503a0c
Miguel <miguel240514@gmail.com>
2025-12-11 19:32:35 +0100

    style: 🚨 apply black and issort

--- app/modules/auth/tests/test_selenium.py
+++ app/modules/auth/tests/test_selenium.py
@@ -1,6 +1,6 @@
 import time
-import pytest
 
+import pytest
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -1,5 +1,5 @@
-from locust import HttpUser, TaskSet, task
 import pytest
+from locust import HttpUser, TaskSet, task
 
 from core.environment.host import get_host_for_locust_testing
 from core.locust.common import get_csrf_token
